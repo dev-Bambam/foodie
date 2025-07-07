a7be7927e61c96c3179801e66aa0efa8935a1b69
dev-Bambam <ayogood18@gmail.com>
2025-07-07 13:42:23 +0100

    I am testing out how copilot agent mode works, and it seems it's great

--- src/api/user/services/admin.service.ts
+++ src/api/user/services/admin.service.ts
@@ -1,30 +1,62 @@
-import * as usertype from '../types/user.types'
-import * as menutype from '../../menu/types/menu.type'
-import * as ordertype from '../../order/types/order.type'
-import { injectable, inject } from 'tsyringe'
+import * as usertype from "../types/user.types";
+import * as menutype from "../../menu/types/menu.type";
+import * as ordertype from "../../order/types/order.type";
+import { IPayment } from "../../payment/types/payment.type";
+import { injectable, inject } from "tsyringe";
 
 @injectable()
-export class AdminService implements usertype.IAdminService{
-    constructor(
-        @inject('IMenuService') private MenuService: menutype.IMenuService,
-        @inject('IOderService') private OrderService: ordertype.IOrderService,
-        @inject('IUserRepo') private UserRepo: usertype.IUserRepository
-    ) { }
-    
-    async createMenu(menuInput: menutype.TCreateMenuInput): Promise<menutype.IMenuItem> {
-        const newMenu = await this.MenuService.createMenu(menuInput)
+export class AdminService implements usertype.IAdminService {
+   constructor(
+      @inject("IMenuService") private MenuService: menutype.IMenuService,
+      @inject("IOderService") private OrderService: ordertype.IOrderService,
+      @inject("IUserRepo") private UserRepo: usertype.IUserRepository
+   ) {}
 
-        return newMenu
-    };
-    async updateMenu(menuId: string, menuInput: menutype.TUpdateMenuItem): Promise<menutype.TMenuItem> {
-        return await this.MenuService.updateMenu(menuId, menuInput)
-    }
-    
-    async getAllCustomers(): Promise<usertype.TCustomer[]> {
-        const customers = await this.UserRepo.fetchAllCustomer()
+   // Authentication
+   async login(input: usertype.TLoginInput): Promise<{ token: string }> {
+      // Implement your admin login logic here
+      // Example: delegate to UserRepo or a dedicated AuthService
+      throw new Error("Not implemented");
+   }
 
-        return customers
-    }
-    
+   // CRUD Order
+   async fetchAllOrder<T extends ordertype.TOrder["status"]>(
+      status?: T
+   ): Promise<ordertype.TOrder[]> {
+      return this.OrderService.fetchAllOrder(status);
+   }
+
+   async updateOrder(
+      orderId: string,
+      orderData: ordertype.TOrderUpdate
+   ): Promise<ordertype.TOrder> {
+      return this.OrderService.updateOrder(orderId, orderData);
+   }
+
+   // CRUD on Menu
+   async createMenu(menuInput: menutype.TCreateMenuInput): Promise<menutype.TMenuItem> {
+      return this.MenuService.createMenu(menuInput);
+   }
+
+   async updateMenu(
+      menuId: string,
+      menuInput: menutype.TUpdateMenuItem
+   ): Promise<menutype.TMenuItem> {
+      return this.MenuService.updateMenu(menuId, menuInput);
+   }
+
+   async deleteMenu(menuId: string): Promise<void> {
+      return this.MenuService.deleteMenu(menuId);
+   }
+
+   // CRUD on Payment
+   async updatePayment(paymentId: string): Promise<IPayment> {
+      // Implement your payment update logic here
+      throw new Error("Not implemented");
+   }
+
+   // CRUD on Customer/ User
+   async getAllCustomers(): Promise<usertype.TCustomer[]> {
+      return this.UserRepo.fetchAllCustomer();
+   }
 }
-
